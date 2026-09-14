9e8b4d4f33e04a00b7677d0f009e570414a7f5c1
柯燕纯 <2245237790@qq.com>
2026-09-14 19:37:33 +0800

    init: 项目初始化，搭建全部函数框架
    完成查重项目基础函数框架，实现ngram文本切分、集合比对、重复率计算逻辑。

---
+++ 3224004305/main.py
@@ -0,0 +1,32 @@
+import sys
+
+def parse_argv():
+    # 解析命令行参数
+    pass
+
+def read_file(file_path):
+    # 读取文件
+    pass
+
+def preprocess(text: str):
+    # 文本预处理
+    pass
+
+def ngram_segment(text: str, n: int):
+    # n‑gram分词
+    pass
+
+def calc_similarity(orig_text: str, copy_text: str, n=2):
+    # 计算重复率
+    pass
+
+def write_answer(out_path: str, rate: float):
+    # 输出结果文件
+    pass
+
+def main():
+    # 主入口函数
+    pass
+
+if __name__ == "__main__":
+    main()
